4e51c5123d07577cbb12823be9665714a8ef9a76
joshchatfield <joshchatfield@gmail.com>
2019-03-07 12:39:21 -0800

    message

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 // create globals
 var database = firebase.database();
-var sample = {
-  firstName: 'yet',
-  lastName: 'another value'
-};
+
+/* --------------------------- NYT FUNCTIONS -------------------- */
+
+// return a NYT api query url
+function getNYTQueryURL(searchTerm) {
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+  var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
+  queryParams.q = searchTerm;
+  return queryURL + $.param(queryParams);
+}
+
+// run ajax call on NYT API
+function getNYTAJAXResult(queryURL) {
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      url: queryURL,
+      method: 'get'
+    }).then(function (result) {
+      resolve(result);
+    });
+  });
+}
+
+// log result of NYT API AJAX
+function displayNYTResult(result) {
+  console.log(result);
+}
+
+/* --------------------------- FIREBASE FUNCTIONS -------------------- */
 
 // create the firebase button
 function createElements(){
@@ -14,11 +39,20 @@ function createElements(){
   $('#firebase-card').append(firebaseButton);
 }
 
+function getObjectToBePushed() {
+  // use this space to get the information to be included in the object
+  var sampleObject = {
+    firstName: 'yet',
+    lastName: 'another value'
+  };
+  return sampleObject;
+}
+
 // when firbase button is clicked push the sample object
 function setUpClickFirebase() {
   $(document).on('click', '#firebase-button', function () {
     console.log('fired');
-    database.ref().push(sample);
+    database.ref().push(getObjectToBePushed());
   });
 }
 
@@ -31,6 +65,8 @@ function setUpFireBaseOnValue() {
     console.log('write failed: ' + error.code)
   })
 }
+
+/* --------------------------- TIMER FUNCTIONS -------------------- */
 
 // what happens every interval event
 function logCurrentTime() {
@@ -49,10 +85,16 @@ function startTimer() {
   }, 1000);
 };
 
+/* --------------------------- MAIN LOGIC -------------------- */
+
 // when document is ready call my functions in proper order
 $(document).ready(function () {
   createElements();
   setUpClickFirebase();
   setUpFireBaseOnValue();
   startTimer();
+  getNYTAJAXResult(getNYTQueryURL('president'))
+    .then(function (result) {
+      displayNYTResult(result)
+    });
 })
